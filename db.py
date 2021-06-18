2a7515811b3d3baafd1b2248bfec909f3a172ed1
Rexwer123 <leorexwer@gmail.com>
2021-06-18 17:58:40 +0300

    modified:   db.py 	modified:   index.py

--- db.py
+++ db.py
@@ -2,25 +2,130 @@ import pymongo
 from colors import colors
 from vininfo import Vin
 
+# #ВОТ ОТСЮДА
+
+testList = [
+    {
+        "VIN": "JN1CV6FE3BM983517",
+        "crashes": [
+            "Наезд на стоящие ТС – 2 августа 2016, Япония",
+            "Столкновение – 10 ноября 2018, Япония"
+        ] 
+    },
+    {
+        "VIN": "1GCCS19W6Y8200480",
+        "crashes": [
+            "Опрокидывание транспортных средств – 8 сентября 2017, Соединенные Штаты Америки"
+        ] 
+    },
+    {
+        "VIN": "WBAPH7C51BE656105",
+        "crashes": [
+           "Наезд на препятствие – 14 марта 2017, Германия"
+        ] 
+    },
+    {
+        "VIN": "JKAZRBC11YA058549",
+        "crashes": [
+           "Наезд на пешехода – 13 августа 2003, Япония",
+           "Столкновение – 4 апреля 2013, Япония"
+        ] 
+    },
+    {
+        "VIN": "5GAKVBKD2EJ144030",
+        "crashes": [
+           "Наезд на велосипедиста – 27 июня 2007, Соединенные Штаты Америки",
+        ] 
+    },
+    {
+        "VIN": "1FBSS31L89DA67372",
+        "crashes": [
+           "Наезд на стоящее транспортное средства – 8 октября 2018, Соединенные Штаты Америки",
+        ] 
+    },
+    {
+        "VIN": "WD2PD644945698571",
+        "crashes": [
+           "Наезд на гужевой транспорт – 13 ноября 2012, Германия", 
+           "Наезд на стоящий ТС – 25 ноября 2012, Германия"
+        ] 
+    },
+    {
+        "VIN": "WBADN6349YGM50573",
+        "crashes": [
+           "Наезд на животных и прочие – 3 июля 2013, Германия", 
+           "Наезд на препятствие – 5сентября 2015, Германия"
+        ] 
+    },
+    {
+        "VIN": "2T2HA31U46C075426",
+        "crashes": [
+           "Удар оторвавшимся колесом – 6 июня 2006, Канада",
+           "(Столкновение – 6 июня 2006, Канада"
+        ] 
+    },
+    {
+        "VIN": "1G1ZC5E06CF303007",
+        "crashes": [
+           "Наезд на стоящее транспортное средство – 14 мая 2008, Соединенные Штаты Америки"
+        ] 
+    },
+    {
+        "VIN": "1G2JB12F837380467",
+        "crashes": [
+           "Столкновение – 23 декабря 2017, Соединенные Штаты Америки"
+        ] 
+    },
+    {
+        "VIN": "2C4RDGCG5FR570605",
+        "crashes": [
+           "Столкновение – 13 января 2016, Канада"
+        ] 
+    },
+    {
+        "VIN": "2B3CA8CT6AH256754",
+        "crashes": [
+           "Столкновение – 13 января 2016, Канада"
+        ] 
+    },
+    {
+        "VIN": "1GC4K0E89FF131170",
+        "crashes": [
+           "Опрокидывание транспортных средств – 14 мая 2019, Соединенные Штаты Америки"
+        ] 
+    },
+    {
+        "VIN": "3FADP4BJ0EM266283",
+        "crashes": [
+           "Наезд на пешехода – 3 июля 2011, Мексика"
+        ] 
+    }
+]
+
+# #ДО СЮДА ПОТОМ УДАЛИТЬ
+
 class DB:
     def __init__(self):
         self.conn = pymongo.MongoClient('localhost', 27017)
-    
-    def addNewCar(self, vin):
         try:
-            car = {"vin": vin.num, "country": vin.country, "region": vin.region, "manufacturer": vin.manufacturer, "years": vin.years}
-            if(self.conn.cars.cars.find_one(car) == None):
-                self.conn.cars.cars.save(car)
+            self.conn.cars.cars.insert_many(testList)
         except Exception as e:
             print(e)
+
+    def assembleResult(self, responseString, crashes):
+        for crash in crashes:
+            responseString += "- " + crash + "\n"
+        return responseString
+
+    def getCarByVIN(self, vinNumber):
+        responseHeader = "VIN: " + vinNumber + "\n" + "Аварии:\n"
+        queryResult = self.conn.cars.cars.find_one({"VIN": vinNumber})
+        if(queryResult):
+            if(len(queryResult["crashes"]) == 0):
+                return False
+            else:
+                return self.assembleResult(responseHeader, queryResult["crashes"])
+        else:
             return False
             
-        return True
-    
-    def getAllCars(self):
-        result = ""
-        for car in self.conn.cars.cars.find({}):
-            result += "VIN: " + car["vin"] + "\n" + "-Country: " + car["country"] + "\n" + "-Region: " + car["region"] + "\n" + "-Years: " + str(car["years"][0])
-        if len(result) == 0:
-            return False
-        return result
+                
--- index.py
+++ index.py
@@ -10,39 +10,27 @@ try:
     print(colors.OKGREEN + 'Connected to the database' + colors.BOLD + ' CARS ' + colors.ENDC + colors.OKGREEN + 'at http://localhost:27017\n' + colors.ENDC)
 except Exception as e:
     print(e)
-    exit()
 
-bot = telebot.TeleBot('API KEY')
-
-def parseInfo(vin):
-    result = "VIN: " + vin.num + "\n"
-    vin = vin.annotate()
-    for attr, value in vin.items():
-        result += "-" + attr + ": " + value + "\n" 
-
-    return result
+bot = telebot.TeleBot('1818558003:AAG1o9g-tGdc-uPcUDl1RAqWh7Vnrce5k7M')
 
 def checkVIN(vinNumber):
-    return Vin(vinNumber).verify_checksum()
-
+    try:
+        return Vin(vinNumber).verify_checksum()
+    except Exception:
+        return False
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if(message.text == '/help'):
-        bot.send_message(message.from_user.id, "Введите VIN, чтобы получить информацию об автомобиле\nЛибо введите /add, чтобы добваить автомобиль по VIN")   
-    if(message.text == '/get'):
-        bot.send_message(message.from_user.id, "Вот, что я смог найти:\n\n" + db.getAllCars())
+    if(message.text == '/help' or message.text == '/start'):
+        bot.send_message(message.from_user.id, "Введите VIN, чтобы получить информацию об автомобиле\n")   
     else:
-        try:
-            if checkVIN(message.text) == True:
-                vin = Vin(message.text)
-                db.addNewCar(vin)
-                vin.annotate()
-                bot.send_message(message.from_user.id, "Вот, что я смог найти:\n\n{0}".format(parseInfo(vin)))
+        if checkVIN(message.text) == True:
+            carInfo = db.getCarByVIN(message.text)
+            if(carInfo == False):
+                bot.send_message(message.from_user.id, "Нет информации о машине с этим VIN")
             else:
-                bot.send_message(message.from_user.id, "Чексумма VIN не совпала, попробуйте другой")
-        
-        except Exception as e:
-            bot.send_message(message.from_user.id, str(e))
+                bot.send_message(message.from_user.id, carInfo)
+        else:
+            bot.send_message(message.from_user.id, "Чексумма VIN не совпала, попробуйте другой")
 
 bot.polling(none_stop=False, interval=0)
